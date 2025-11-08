3ae96329fe76fec959c88b0fe2682641a2739671
Ardhiyan Pratama <ardhiyanp@gmail.com>
2025-11-08 19:48:17 +0700

    fix : error not defined

--- api/models/review.js
+++ api/models/review.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Department.hasMany(models.Employee, { foreignKey: 'departmentId' });
     }
   }
   Reviews.init({
