99861ac72c4ea6b90e8e3ba9c33c2b3ff914a832
Ardhiyan Pratama <ardhiyanp@gmail.com>
2025-11-20 15:25:33 +0700

    fix : cannot find modules in server

--- api/index.js
+++ api/index.js
@@ -14,15 +14,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/api', require('./routes/employeeRoutes'));
-app.use('/api', require('./routes/departementRoutes'));
-app.use('/api', require('./routes/hardcompetenciesRoutes'));
-app.use('/api', require('./routes/softcompetenciesRoutes'));
 
 
 (async () => {
     try {
         await initDatabase();
+
+        app.use('/api', require('./routes/employeeRoutes'));
+        app.use('/api', require('./routes/departementRoutes'));
+        app.use('/api', require('./routes/hardcompetenciesRoutes'));
+        app.use('/api', require('./routes/softcompetenciesRoutes'));
 
         app.get('/', (req, res) => {
             res.send('Hello from Express API!');
